c83e14135f200fc8d35c010bbae53e9c024d0759
Zaray <cor191025@uvg.edu.gt>
2021-05-22 20:52:34 -0600

    fixing errors

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -5,7 +5,6 @@ function Login() {
   return (
     <div className="d-flex justify-content-around bg-secondary vw-100 h-100">
       <LoginFormContainer />
-      {/* <img src={test} className={'ml-5 d-none d-lg-block'}/> */}
     </div>
   );
 }
