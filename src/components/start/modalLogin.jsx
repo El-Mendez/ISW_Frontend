8a0c1acb0155e3ad5027700d2bf520cfd864689b
Zaray <cor191025@uvg.edu.gt>
2021-08-03 01:03:06 -0600

    Mejora en la interfaz de logIn, envío de datos para request

--- src/components/start/modalLogin.jsx
+++ src/components/start/modalLogin.jsx
@@ -4,32 +4,33 @@ import GoogleLogin from 'react-google-login';
 import Axios from 'axios';
 import { useForm } from 'react-hook-form';
 import history from '../history';
+import logo from '../../assets/logo.svg';
 
 
 import { zodResolver } from '@hookform/resolvers/zod';
 import * as z from 'zod';
 
 const schema = z.object({
-    username: z.string().nonempty({ message: 'Ingresa un usuario' }),
-    password: z.string().nonempty({ message: 'Ingresa una contraseña' }).min(8, { message: 'Mínimo 8 caracteres' }),
+    carne: z.number().min(3, {message: 'EL mínimo de un carné UVG es de 3 dígitos'}),
+    password: z.string().nonempty({ message: 'Ingrese una contraseña' }).min(8, { message: 'Mínimo 8 caracteres' }),
 });
 
 
 export default function ModalLogin(props) {
 
-    const get_user = 'http://api.meetinguvg.me/api/login/';
+    const get_user = 'http://api.meetinguvg.me/free/login';
 
     const { register, handleSubmit, formState: { errors } } = useForm({
         resolver: zodResolver(schema),
     });
 
     const [user, setUser] = useState({
-        username: '',
+        carne: 0,
         password: '',
     });
 
     const [filled, setFilled] = useState({
-        username: false,
+        carne: false,
         password: false,
     });
 
@@ -39,7 +40,7 @@ export default function ModalLogin(props) {
             try {
                 const { data } = await Axios.post(get_user,
                     {
-                        username: user.username,
+                        carne: user.carne,
                         password: user.password
                     }
                 );
@@ -89,6 +90,14 @@ export default function ModalLogin(props) {
         getUser();
     };
 
+    const cleanUp = () => {
+        setUser({
+            carne: 0,
+            password: ''
+        })
+        props.onHide()
+    }
+
 
   return (
       <Modal
@@ -98,58 +107,78 @@ export default function ModalLogin(props) {
           centered>
         <Modal.Header className="bg-purple-dark">
           <Modal.Title id="contained-modal-title-vcenter">
-                  <p className="modalHeader my-2">
-                      INICIAR SESIÓN
+              <div className="d-flex flex-column align-items-center">
+                  <p className="welcome">
+                      BIENVENIDO A
                   </p>
+                  <img src={logo} alt="Logo" className="img-size w-50"/>
+                  <p className="mt-1 description text-center px-4">
+                      Inicia sesión y disfruta de una nueva experiencia mientras haces nuevos amigos
+                  </p>
+              </div>
           </Modal.Title>
-            <button type="button" className="btn-close m-0" aria-label="Close" onClick={props.onHide}/>
+            <button type="button" className="btn-close m-0" aria-label="Close" onClick={cleanUp}/>
         </Modal.Header>
         <Modal.Body>
-            <div className="container">
-                {/* G */}
+
+            <div className="auth-container">
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    {/* Username */}
+                    <div className="input-container">
+                         <span className={`material-icons input-icon ${filled.carne ? 'is-filled' : ' '}`}>
+                            person
+                        </span>
+                        <input
+                            className={`input ms-1 ${filled.carne ? 'is-filled' : ' '}`}
+                            type="number"
+                            name="carne"
+                            placeholder="Número de carné"
+                            onInput={handleInputChange}
+                            {...register('carne')}
+                        />
+                    </div>
+                    <small className="text-danger text-small d-block mb-2">
+                        {/* <Exclamation_icon/> */}
+                        {errors.carne?.message}
+                    </small>
+                    {/* Password */}
+                    <div className="input-container mt-3">
+                         <span className={`material-icons input-icon ${filled.password ? 'is-filled' : ' '}`}>
+                            lock
+                        </span>
+                        <input
+                            className={`input ms-1 ${filled.password ? 'is-filled' : ' '}`}
+                            type="password"
+                            name="password"
+                            placeholder="Contraseña"
+                            onInput={handleInputChange}
+                            {...register('password')}
+                        />
+                    </div>
+                    <small className="text-danger text-small d-block mb-2">
+                        {/* <Exclamation_icon/> */}
+                        {errors.password?.message}
+                    </small>
+                    {/* LOGIN BUTTON */}
+                    <div className="d-flex justify-content-between align-items-center mt-4 px-2">
+                        <a>¿Olvidaste tu contraseña?</a>
+                        <button onSubmit={onSubmit} className="btn-fill arrow-button">INICIAR SESIÓN
+                            <span
+                                className="material-icons position-absolute ms-1">arrow_forward</span>
+                        </button>
+                    </div>
+                </form>
+                <span className="divider">○</span>
+                {/* Google */}
                 <div>
                     <GoogleLogin
                         clientId= "405374042535-g3sqooe9ncnj7lm394iu27u9vd99ma16.apps.googleusercontent.com"
-                        buttonText = "Ingresar con Google"
+                        buttonText = "Continúa con Google"
                         onSuccess={googleLogin}
                         onFailure={googleLogin}
                         cookiePolicy={'single_host_origin'}
                     />
                 </div>
-                <form onSubmit={handleSubmit(onSubmit)}>
-                    {/* Username */}
-                    <div className="position-relative mt-2">
-                        <input
-                            className={`input ${filled.username ? 'is-filled' : ' '}`}
-                            type="text"
-                            name="username"
-                            onInput={handleInputChange}
-                            {...register('username')}
-                        />
-                        <label className="label">Usuario</label>
-                        <small className="text-danger text-small d-block mb-2">
-                            {/* <Exclamation_icon/> */}
-                            {errors.username?.message}
-                        </small>
-                    </div>
-                    {/* Password */}
-                    <div className="position-relative mt-2">
-                        <input
-                            className={`input ${filled.username ? 'is-filled' : ' '}`}
-                            type="password"
-                            name="password"
-                            onInput={handleInputChange}
-                            {...register('password')}
-                        />
-                        <label className="label">Contraseña</label>
-                        <small className="text-danger text-small d-block mb-2">
-                            {/* <Exclamation_icon/> */}
-                            {errors.password?.message}
-                        </small>
-                    </div>
-                    {/* LOGIN BUTTON */}
-                    <button onSubmit={onSubmit} className="btn btn-meeting btn-fill my-3 w-100">INICIA SESIÓN</button>
-                </form>
             </div>
         </Modal.Body>
       </Modal>
