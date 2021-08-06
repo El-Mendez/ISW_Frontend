98d594470c8e1d2efae529b8fa19c4020caebc26
Zaray <cor191025@uvg.edu.gt>
2021-08-06 09:34:38 -0600

    importando rutas

--- src/components/auth/login.jsx
+++ src/components/auth/login.jsx
@@ -39,7 +39,7 @@ export default function Login(props) {
         console.log("Loading...");
         const fetchData = async () => {
             try {
-                const { data } = await Axios.post('http://api.meetinguvg.me/free/login',
+                const { data } = await Axios.post(LOGIN,
                     {
                         carne: user.carne,
                         password: user.password
--- src/components/utils/search.jsx
+++ src/components/utils/search.jsx
@@ -5,38 +5,27 @@ import { SEARCH_CAREER } from './rutas';
 
 export default function Search(career) {
 
-  // function search(){
-  //   console.log("Loading...");
-  //   const request = async () => {
-  //     try {
-  //       const res = await axios.get(SEARCH_CAREER,
-  //           {
-  //             params:{
-  //               nombre: 'ing'
-  //             }
-  //           }
-  //       );
-  //       console.log(res)
-  //     } catch (error) {
-  //       console.log(error);
-  //     }
-  //   };
-  //   request();
-  // };
+  function search(){
+    console.log("Loading...");
+    const request = async () => {
+      try {
+        const res = await axios.get(SEARCH_CAREER,
+            {
+              params:{
+                nombre: 'ing'
+              }
+            }
+        );
+        console.log(res)
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    request();
+  };
 
   useEffect( () => {
-   axios({
-     method: 'GET',
-     url: SEARCH_CAREER,
-     params:{
-       nombre: career
-     }
-   }).then(res => {
-     console.log(res.data)
-   }).catch((error) => {
-       console.log('what')
-       console.log(error)
-   })
+   search();
   }, [career]);
 
     return null
