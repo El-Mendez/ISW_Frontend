b8d4f7509e73060f55afa3861b51918900e003e5
Orlando <orlando.osberto@gmail.com>
2021-08-06 15:25:57 -0600

    Eliminando la cookie de inicio de sesión

--- src/components/navbar/services.jsx
+++ src/components/navbar/services.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import user from '../../assets/user.png';
 import { Link, useRouteMatch, useParams } from 'react-router-dom';
+import Cookies from 'universal-cookie';
+import history from '../history';
 
 function Services() {
   const { url } = useRouteMatch();
+  const cookies = new Cookies();
+  function logout() {
+    cookies.remove('session')
+    history.push(`/`);
+    history.go();
+  }
   return (
     <div className="d-flex align-items-center">
       {/* Search */}
@@ -19,11 +27,11 @@ function Services() {
         </span>
       </div>
       <Link to="/" className="noDecorations">
-      <div className="services " style={{ height: '24px' }}>
-        <span className="material-icons">
-          directions_run
-        </span>
-      </div>
+        <div className="services " style={{ height: '24px' }} onClick={logout}>
+          <span className="material-icons">
+            directions_run
+          </span>
+        </div>
       </Link>
 
       {/* User image profile */}
