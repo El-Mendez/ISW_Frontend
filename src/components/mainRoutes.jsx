90bcbe1422e1f013dcb28a44a5de8b7f0efc1a3f
Zaray <cor191025@uvg.edu.gt>
2021-08-18 19:44:33 -0600

    forgot password page

--- src/components/mainRoutes.jsx
+++ src/components/mainRoutes.jsx
@@ -8,6 +8,7 @@ import Axios from "axios";
 import { AUTH } from './utils/rutas'
 import Register from "./auth/register/register";
 import Dashboard from "./dashboard/dashboard";
+import Forgot_password from "./password/forgot_password";
 
 
 export default function MainRoutes() {
@@ -52,6 +53,8 @@ export default function MainRoutes() {
         <Route path="/home" component={Dashboard} />
         <Route path="/data" component={UserInfo} />
         <Route path="/perfil" component={UserInfo} />
+        <Route path="/recovery-password" component={Forgot_password} />
+        <Route path="/recovery/:token" component={UserInfo} />
       </Switch>
     </Router>
   );
--- src/components/utils/rutas.jsx
+++ src/components/utils/rutas.jsx
@@ -11,4 +11,8 @@ const AUTH = 'http://api.meetinguvg.me/auth/ping';
 const ASSIGN_SECTION = 'http://api.meetinguvg.me/auth/seccion';
 const ASSIGN_HOBBY = 'http://api.meetinguvg.me/auth/hobby';
 
-module.exports = {LOGIN, SIGNUP, SEARCH_CAREER, SEARCH_COURSE, SEARCH_HOBBY, AUTH, ASSIGN_SECTION, ASSIGN_HOBBY}
+const PASSWORD_RESET = 'http://api.meetinguvg.me/request/passwordReset'
+const ACCEPT_PASSWORD_RESET = 'http://api.meetinguvg.me/request/acceptPasswordRese'
+
+module.exports = {LOGIN, SIGNUP, SEARCH_CAREER, SEARCH_COURSE, SEARCH_HOBBY, AUTH, ASSIGN_SECTION, ASSIGN_HOBBY,
+PASSWORD_RESET, ACCEPT_PASSWORD_RESET}
