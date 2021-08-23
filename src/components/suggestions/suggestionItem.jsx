7d782c18f894fab25ffb436456a87561c8b5a572
Orlando <orlando.osberto@gmail.com>
2021-08-22 22:42:36 -0600

    Mejorando el diseño de las recomendaciones

--- src/components/suggestions/suggestionItem.jsx
+++ src/components/suggestions/suggestionItem.jsx
@@ -1,21 +1,28 @@
 import React from 'react';
-
+import {MdClass} from 'react-icons/md'
+import {AiFillCustomerService, AiFillContacts} from 'react-icons/ai'
 function SuggestionItem(props) {
   const item = props;
   return (
-    <div className="container">
+    <div className="container suggestions">
       <div className="row align-items-center">
         <div className="col-4 ">
           <span className="material-icons suggestionsImages ">
             account_circle
           </span>
         </div>
-        <div className="col datos">
+        <div className="col suggestions">
           <div className="row align-items-start">
             <h1>{item.nombre +" "+ item.apellido}</h1>
           </div>
           <div className="row align-items-start">
-            <h2>{item.apellido.substring(0, 3)+item.carne+"@uvg.edu.gt"}</h2>
+            <h2>{item.apellido.substring(0, 3)+item.carne+"@uvg.edu.gt"}
+            </h2>
+            <div className="infoser">
+              <MdClass className="infoUser"/>
+              <AiFillCustomerService className="infoUser"/>
+              <AiFillContacts className="infoUser"/>
+            </div>
           </div>
         </div>
       </div>
