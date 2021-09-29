679cae43e1500bbeaa44dd585486bd10e00e95a0
eerh <eduardorh1312@gmail.com>
2021-09-28 21:23:25 -0600

    dashboard

--- src/components/dashboard/dashcontent.jsx
+++ src/components/dashboard/dashcontent.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './dashboard.scss';
-
+import user from '../../assets/usericon.png';
 function DashContent(){
     return(
 
@@ -10,9 +10,28 @@ function DashContent(){
             </div>
 
             <div className="rowd1">
-                
-                <div className="r1">Hobbies</div>
-                <div className="r1">Amigos</div>
+                <div className="r1">
+                    <img src={user} alt="user" className="userimg" />
+                    <div className="cr1">
+                        <p className="crr1">Juan Ramos</p>
+                        <p className="crr2">#20187</p>
+                        <p className="crr2">Ingenieria Mecatronica</p>
+                        <p className="crr2">2do año, 4to Semestre</p>
+
+                    </div>
+                </div>
+                <div className="r2">
+
+                </div>
+            </div>
+
+            <div className = "rowd2">
+                <div className="r3">
+
+                </div>
+                <div className="r3">
+
+                </div>
             </div>
         </section>
     );
