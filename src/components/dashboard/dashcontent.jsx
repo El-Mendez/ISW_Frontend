1cfd59d197e4d8dc0a497ff2c357640ef4d6b6ca
eerh <eduardorh1312@gmail.com>
2021-09-30 21:50:52 -0600

    rediseño de dashboard en proceso

--- src/components/dashboard/dashcontent.jsx
+++ src/components/dashboard/dashcontent.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import './dashboard.scss';
-import user from '../../assets/usericon.png';
 
 function DashContent() {
   return (
@@ -11,16 +10,7 @@ function DashContent() {
       </div>
 
       <div className="rowd1">
-        <div className="r1">
-          <img src={user} alt="user" className="userimg" />
-          <div className="cr1">
-            <p className="crr1">Juan Ramos</p>
-            <p className="crr2">#20187</p>
-            <p className="crr2">Ingenieria Mecatronica</p>
-            <p className="crr2">2do año, 4to Semestre</p>
-
-          </div>
-        </div>
+        <div className="r1" />
         <div className="r2" />
       </div>
 
