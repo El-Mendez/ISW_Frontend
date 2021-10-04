6de5717faece48f96375f2a125113f2f48de1e1e
Zaray <cor191025@uvg.edu.gt>
2021-10-04 16:26:44 -0600

    fixing timing errors in e2e testing

--- src/test/system/index.test.js
+++ src/test/system/index.test.js
@@ -6,22 +6,12 @@ const url = 'http://localhost:8080/';
 const driver = new Builder().forBrowser('firefox').build();
 
 describe('testing', () => {
-  jest.setTimeout(100000000000);
+  jest.setTimeout(1000000000);
 
   beforeAll(() => {
     // Access to the website
     driver.get(url);
   });
-
-  // it('Redirect to Sign Up page', async () => {
-  //   driver.navigate().to('http://meetinguvg.me/signUp');
-  //   await driver.findElement(By.name('carne')).sendKeys('19707');
-  //   await driver.findElement(By.name('correo')).sendKeys('191025');
-  //   await driver.findElement(By.name('nombre')).sendKeys('191025');
-  //   await driver.findElement(By.name('apellido')).sendKeys('191025');
-  //   await driver.findElement(By.name('carreraId')).getAttribute('value');
-  //   await driver.findElement(By.name('password')).sendKeys('123456789');
-  // });
 
   it('Log in', async () => {
     await driver.findElement(By.id('logInRequest')).click();
@@ -29,11 +19,13 @@ describe('testing', () => {
     await driver.findElement(By.name('password')).sendKeys('123456789');
     await driver.findElement(By.id('logIn')).click();
     jest.setTimeout(100);
+    driver.wait(until.elementLocated(By.id('dash-container')), 100);
     const element = await driver.findElement(By.id('dash-container')).isDisplayed();
     expect(element).toBeTruthy();
   });
 
   it('Look up for courses recommendations', async () => {
+    jest.setTimeout(200);
     await driver.findElement(By.id('recommendations')).click();
     driver.wait(until.elementLocated(By.id('coursesRecommendations')), 100);
     await driver.findElement(By.id('coursesRecommendations')).click();
@@ -41,11 +33,17 @@ describe('testing', () => {
     expect(suggestion).toBeTruthy();
   });
 
-  // it('Send friendship request', async () => {
-  //   await driver.findElement(By.xpath("//div[@value='Diego']")).click();
-  //   driver.wait(until.elementLocated(By.id('profileContainer')));
-  //   await driver.findElement(By.id('addFriend')).click();
-  // });
+  it('Send friendship request', async () => {
+    jest.setTimeout(100);
+    await driver.findElement(By.xpath("//div[@value='Maria']")).click();
+    driver.wait(until.elementLocated(By.id('profileContainer')));
+    await driver.findElement(By.id('addFriend')).click();
+    jest.setTimeout(100);
+    await driver.findElement(By.id('mailBox')).click();
+    await driver.findElement(By.id('sentRequest')).click();
+    const sent = await driver.findElement(By.xpath("//div[@value='19822']"));
+    expect(sent).toBeTruthy();
+  });
 
   it('Check the actual friends', async () => {
     jest.setTimeout(100);
@@ -61,13 +59,6 @@ describe('testing', () => {
     const element = await driver.findElement(By.id('profileContainer')).isDisplayed();
     expect(element).toBeTruthy();
   });
-
-  // it('Look up for hobbies recommendations', async () => {
-  //   await driver.findElement(By.id('recommendations')).click();
-  //   await driver.findElement(By.id('hobbiesRecommendations')).click();
-  //   const suggestion = await driver.findElement(By.id('suggestionResult')).isDisplayed();
-  //   expect(suggestion).toBeTruthy();
-  // });
 
   afterEach(() => {
     cleanup();
