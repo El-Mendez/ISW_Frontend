677b5124128d829fd3578550176ff9eba757c168
Zaray <cor191025@uvg.edu.gt>
2021-11-22 12:00:51 -0600

    adding log out after getting the suggestions

--- src/test/system/cases/suggestions.js
+++ src/test/system/cases/suggestions.js
@@ -4,6 +4,7 @@ import { cleanup } from '@testing-library/react';
 import { By, until } from 'selenium-webdriver';
 import Login from '../pages/login';
 import Recommendations from '../pages/suggestions';
+import Logout from '../pages/logout';
 import BaseConfig from '../base_config';
 
 export default class Suggestions extends BaseConfig {
@@ -23,6 +24,7 @@ export default class Suggestions extends BaseConfig {
         await this.openPage(`http://${this.host}/`);
         this.login = await new Login(this.driver);
         this.recommendations = await new Recommendations(this.driver);
+        this.logout = await new Logout(this.driver);
       }, 100000);
 
       test('Log in', async () => {
@@ -35,6 +37,12 @@ export default class Suggestions extends BaseConfig {
         await this.recommendations.suggestionsAction(this.type);
         const suggestion = await this.driver.wait(until.elementLocated(By.id('suggestionResult')), 10000);
         expect(suggestion).toBeTruthy();
+      }, 100000);
+
+      test('Log out', async () => {
+        await this.logout.logoutAction();
+        await this.driver.wait(until.elementLocated(By.id('home')), 10000);
+        expect(await this.driver.getCurrentUrl()).toEqual(`http://${this.host}/`);
       }, 100000);
 
       beforeEach(() => {
